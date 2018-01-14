4ec1d0364b7e38125b50e999b0014c8281af42b9
shivijd <35404094+shivijd@users.noreply.github.com>
2018-01-14 23:50:14 +0530

    Add files via upload

---
+++ ti.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n<0)
+{
+	printf("number is negative");
+}
+else
+{
+	printf("number is positive");
+}
+}
