5e1d8f079c0950cc7bbe756e2594147946a3656f
shivijd <35404094+shivijd@users.noreply.github.com>
2018-01-15 00:03:46 +0530

    Add files via upload

---
+++ t3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char n;
+printf("enter the number");
+scanf("%s",&n);
+if(n=='a'||'e'||'i'||'o'||'u')
+{
+	printf("number is vowel");
+}
+else
+{
+	printf("number is consonent");
+}
+}
