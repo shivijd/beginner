0c95dcb3c0ec613a3fbf03229fda5ca3a05c4060
shivijd <35404094+shivijd@users.noreply.github.com>
2018-01-15 23:57:09 +0530

    Add files via upload

---
+++ t4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	char ch;
+	printf("enter the character");
+	scanf("%c",&ch);
+	if((ch>'a' && ch<'z')||(ch>'A' && ch<'Z'))
+	{
+		printf("alphabet");
+	}
+	else
+	{
+		printf("not");
+	}
+}
