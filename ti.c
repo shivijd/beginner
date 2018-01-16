3b17a5a900c26cef0963e6242a61559b54557a39
shivijd <35404094+shivijd@users.noreply.github.com>
2018-01-16 22:13:21 +0530

    Update ti.c

--- ti.c
+++ ti.c
@@ -4,11 +4,15 @@ void main()
 int n;
 printf("enter the number");
 scanf("%d",&n);
+if(n==0)
+{
+  printf("zero");
+}
 if(n<0)
 {
 	printf("number is negative");
 }
-else
+else if(n>0 && n<100000)
 {
 	printf("number is positive");
 }
