c294a4899c3d6af4e40dae8115d2ff48ad5c5cdd
shivijd <35404094+shivijd@users.noreply.github.com>
2018-01-22 01:15:29 +0530

    Add files via upload

---
+++ palin.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int i, n,orgno,rem,rev=0;
+	printf("enter the number");
+	scanf("%d",&n);
+	orgno=n;
+	while(orgno!=0)
+	{
+	    rem= orgno%10;
+        rev= rev*10 + rem;
+        orgno =orgno/10;
+}
+if(rev==n)
+{
+printf("number is palindrome");
+}
+else
+{
+	printf("not palindrome");
+}
+}
