25248ad421ef17cbf700cb3d88d26a5746600232
shivijd <35404094+shivijd@users.noreply.github.com>
2018-01-26 22:52:50 +0530

    Add files via upload

---
+++ min.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int a[10],i,n,min;
+	printf("enter the no of elements");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	min=a[0];
+	for(i=0;i<n;i++)
+	{
+		if(a[i]<min)
+		{
+			min=a[i];
+		}
+		
+	}
+	printf("%d",min);
+}
